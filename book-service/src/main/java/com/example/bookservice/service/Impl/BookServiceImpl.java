f36c3c3f770542b86d307eee93b597cb397f6cd7
Andrey <andreygluh@yandex.ru>
2024-08-08 15:09:26 +0300

    5.1 Add package(Class): Controller(BookController) Repository(BookRePo) Service(BookService, Boo kServiceImpl)

---
+++ book-service/src/main/java/com/example/bookservice/service/Impl/BookServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.bookservice.service;
+
+import com.example.bookservice.repository.BookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl {
+
+    public
+}
